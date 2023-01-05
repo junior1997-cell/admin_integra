4a459ef02856857b446755e0f67b63bf97cadbe2
David Requejo Santacruz <davidrequejo@upeu.edu.pe>
2023-01-05 16:53:33 -0500

    footer pagess

--- vistas/footer.php
+++ vistas/footer.php
@@ -1,6 +1,7 @@
 <!-- Footer -->
 <footer class="main-footer">
-  <strong id="copyright">Copyright &copy; 2021 - <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script> <a href="escritorio.php">SevensIngenieros.SAC</a>.</strong>
+  <strong id="copyright">Copyright &copy; 2022 - <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script> 
+  <a href="https://www.facebook.com/profile.php?id=100086343481837"> JDL Technology</a>.</strong>
   Todos los derechos reservados  .
   <div class="float-right d-none d-sm-inline-block"><b>Version</b> 0.0.1</div>
 </footer>
